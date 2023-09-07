e58ed7732e58b064e901be94a59ca36c9536d6e6
graphsoluae <128033359+graphsoluae@users.noreply.github.com>
2023-09-07 11:22:49 +0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,25 +1,74 @@
-$(document).ready(function () {
-    // Listen for click events on category buttons
-    $('.category-btn').click(function () {
-        // Remove active class from all buttons
-        $('.category-btn').removeClass('active');
-        // Add active class to the clicked button
-        $(this).addClass('active');
+// $(document).ready(function () {
+//     // Listen for click events on category buttons
+//     $('.category-btn').click(function () {
+//         // Remove active class from all buttons
+//         $('.category-btn').removeClass('active');
+//         // Add active class to the clicked button
+//         $(this).addClass('active');
 
-        // Get the target section ID from the data attribute
-        var target = $(this).data('target');
+//         // Get the target section ID from the data attribute
+//         var target = $(this).data('target');
 
-        // Scroll to the target section
-        $('html, body').animate({
-            scrollTop: $('#' + target).offset().top
-        }, 500);
+//         // Scroll to the target section
+//         $('html, body').animate({
+//             scrollTop: $('#' + target).offset().top
+//         }, 500);
 
-        // Move scrollbar thumb to the clicked button
-        var buttonPosition = $(this).position().left;
-        $('.scrollbox').animate({
-            scrollLeft: buttonPosition
-        }, 400);
-    });
+//         // Move scrollbar thumb to the clicked button
+//         var buttonPosition = $(this).position().left;
+//         $('.scrollbox').animate({
+//             scrollLeft: buttonPosition
+//         }, 400);
+//     });
     
+// });
+
+
+
+var scrollbox = document.querySelector(".scrollbox");
+
+
+$(document).ready(function () {
+    // Listen for scroll events on the body
+    $(window).scroll(function () {
+        // Check the scroll position
+        var scrollPosition = $(window).scrollTop();
+
+
+        $('.category-btn').each(function () {
+
+            var target = $(this).data('target');
+
+            var sectionOffset = $('#' + target).offset().top;
+
+            
+            if (scrollPosition >= sectionOffset) {
+
+                $('.category-btn').removeClass('active');
+
+                $(this).addClass('active');
+
+
+            }
+
+
+            var activeElement = document.querySelector('.category-btn.active');
+            if (activeElement) {
+                var activeElementRect = activeElement.getBoundingClientRect();
+                var scrollboxRect = scrollbox.getBoundingClientRect();
+
+                var scrollLeftOffset = activeElementRect.left - scrollboxRect.left;
+                scrollbox.scrollLeft += scrollLeftOffset;
+            }
+
+        });
+
+
+
+
+    });
 });
+
+
+
 
